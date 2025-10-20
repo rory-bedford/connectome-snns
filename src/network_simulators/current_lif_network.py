:bug: Surrogate gradient backwards pass cant be stateful

src/network_simulators/current_lif_network.py
@@ -73,9 +73,6 @@ class CurrentLIFNetwork(CurrentLIFNetwork_IO):
         beta_E = torch.exp(-dt / self.tau_mem_E)
         beta_I = torch.exp(-dt / self.tau_mem_I)
 
-        # Initialise surrogate gradient non-linearity
-        spike_fn = SurrGradSpike(self.surrgrad_scale)
-
         # Default initial membrane potentials to resting potential if not provided
         if initial_v is None:
             initial_v = torch.zeros(
@@ -178,8 +175,8 @@ class CurrentLIFNetwork(CurrentLIFNetwork_IO):
             )
 
             # Generate spikes based on threshold - uses surrogate gradient
-            s_exc = spike_fn(v_exc - self.theta_E)
-            s_inh = spike_fn(v_inh - self.theta_I)
+            s_exc = self.spike_fn(v_exc - self.theta_E)
+            s_inh = self.spike_fn(v_inh - self.theta_I)
 
             # Reset neurons that spiked
             v_exc = v_exc * (1 - s_exc) + self.U_reset_E * s_exc

src/optimisation/surrogate_gradients.py
@@ -11,23 +11,17 @@ class SurrGradSpike(torch.autograd.Function):
     as this was done in Zenke & Ganguli (2018).
     """
 
-    def __init__(self, scale=100.0):
-        """
-        Args:
-            scale: Scaling factor for the surrogate gradient fast sigmoid.
-        """
-        super().__init__()
-        self.scale = scale
-
     @staticmethod
-    def forward(ctx, input):
+    def forward(ctx, input, scale):
         """
         In the forward pass we compute a step function of the input Tensor
         and return it. ctx is a context object that we use to stash information which 
         we need to later backpropagate our error signals. To achieve this we use the 
-        ctx.save_for_backward method.
+        ctx.save_for_backward method. We also need to pass the scale parameter here
+        for use in the backward pass.
         """
         ctx.save_for_backward(input)
+        ctx.scale = scale
         out = torch.zeros_like(input)
         out[input > 0] = 1.0
         return out
@@ -42,5 +36,5 @@ class SurrGradSpike(torch.autograd.Function):
         """
         input, = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad = grad_input/(SurrGradSpike.scale*torch.abs(input)+1.0)**2
+        grad = grad_input/(ctx.scale*torch.abs(input)+1.0)**2
         return grad