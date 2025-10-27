:bug: Broadcasting error

src/network_simulators/conductance_lif_network.py
@@ -180,12 +180,13 @@ class ConductanceLIFNetwork(ConductanceLIFNetwork_IO):
 
             # Compute currents
             I = (
-                g.sum(dim=2) * (v - self.E_syn)
+                g.sum(dim=2) * (v[:, None] - self.E_syn[None, :])
             )  # Conductance times driving force (conductance is difference of rise and decay here)
-            I_FF = g_FF.sum(dim=2) * (v - self.E_syn_FF) if inputs is not None else 0
-
-            if inputs is not None:
-                I += g_FF.sum(dim=2) * (v - self.E_syn_FF)
+            I_FF = (
+                g_FF.sum(dim=2) * (v[:, None] - self.E_syn_FF[None, :])
+                if inputs is not None
+                else 0
+            )
 
             # Update membrane potentials (without reset)
             v = (