27494a1fcb65271c904f61e89f1e00283cc13e42
Rory Bedford <rorybedford@protonmail.com>
2025-10-28 10:45:46 +0100

    :bug: Debugging with print statements

--- src/network_simulators/conductance_lif_network.py
+++ src/network_simulators/conductance_lif_network.py
@@ -213,8 +213,8 @@ class ConductanceLIFNetwork(ConductanceLIFNetwork_IO):
             v = (
                 self.E_L  # Resting potential
                 + (v - self.E_L) * beta  # Leak
-                + I.sum(dim=2) * dt / self.C_m  # Recurrent current
-                + (
+                - I.sum(dim=2) * dt / self.C_m  # Recurrent current
+                - (
                     I_FF.sum(dim=2) * dt / self.C_m if inputs is not None else 0
                 )  # Feedforward current
             )
